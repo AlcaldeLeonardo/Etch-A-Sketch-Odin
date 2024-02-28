133611134ce9bf93ba60ec7ba79d3b94455795a8
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-02-28 18:17:17 -0300

    whiteboard size configuration popup added

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,21 @@ import { Drawing } from "./modules/draw.js";
 import { RenderSketch } from "./modules/render.js"
 
 (()=>{
+
+    const btnSizeConfig = document.querySelector(`#btnSize`);
+    const popup = document.querySelector(`.darkness`);
+    const btnSetConfirm = document.querySelector(`#btnSizeConfig`)
+
+    btnSizeConfig.addEventListener(`click`, ()=> {
+        popup.classList.toggle(`nonVisible`);
+        console.log("Hi, i'm the popup")
+    })
+    btnSetConfirm.addEventListener(`click`, ()=> {
+        popup.classList.toggle(`nonVisible`);
+        console.log("Bye");
+    })
+
+    
     RenderSketch(16, 16);
     Drawing();
 })()
