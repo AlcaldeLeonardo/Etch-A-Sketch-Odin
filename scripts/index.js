a1d82266a96c6254ce17a970ce6834bffa9a8a05
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-02-28 19:07:06 -0300

    I separated the function from the index configuration popup and passed it to a module

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,12 @@
 import { Drawing } from "./modules/draw.js";
 import { RenderSketch } from "./modules/render.js"
+import { SetWhiteboardPopup } from "./modules/setWhitboard.js";
 
 (()=>{
 
     const container = document.querySelector(`.container`)
 
-    const btnSizeConfig = document.querySelector(`#btnSize`);
-    const popup = document.querySelector(`.darkness`);
-    const btnSetConfirm = document.querySelector(`#btnSizeConfig`)
-    const xDimension = document.querySelector("#Xdimension")
-    const yDimension = document.querySelector("#Ydimension")
-
-    btnSizeConfig.addEventListener(`click`, (e)=> {
-        e.preventDefault();
-        popup.classList.toggle(`nonVisible`);
-        console.log("Hi, i'm the popup")
-    })
-    btnSetConfirm.addEventListener(`click`, (e)=> {
-        let x = xDimension.value;
-        let y = yDimension.value;
-
-        e.preventDefault();
-        while(container.firstChild){
-            container.removeChild(container.firstChild)
-        }
-
-        RenderSketch(container, Number(x), Number(y))
-        popup.classList.toggle(`nonVisible`);
-        
-        console.log("Bye");
-        Drawing();
-    })
-
-
     RenderSketch(container, 16, 16);
+    SetWhiteboardPopup(container);
     Drawing();
 })()
