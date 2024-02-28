ee5ca461d25fbecc08695342415acd1f65f399ad
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-02-28 17:31:24 -0300

    Pixel dimensions
    Now the dimension of each div, in addition to the number of cells, is managed by the render function

--- scripts/modules/render.js
+++ scripts/modules/render.js
@@ -2,11 +2,13 @@ export function RenderSketch(x, y){
     const container = document.querySelector(`.container`)
     for (let i = 0; i < y; i++) {
         const column = document.createElement(`div`)
+        column.style.width = `calc(100%/${y})`
         column.className = `column`
         container.appendChild(column);
         for (let j = 0; j < x; j++){
             const square = document.createElement(`div`)
             square.className = `square`
+            square.style.height = `calc(100%/${x})`
             column.appendChild(square);
         }
         
