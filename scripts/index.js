5f587b8158eb403015a454a3e4522f8588337e00
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-03-01 16:15:55 -0300

    btnClear added
    button that allows you to delete the pizza machine and return to default settings

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,19 @@ import { SetWhiteboardPopup } from "./modules/setWhiteboard.js";
 (()=>{
 
     const container = document.querySelector(`.container`)
+    const btnClear = document.querySelector(`#btnClear`)
 
+    
     RenderSketch(container, 16, 16);
     SetWhiteboardPopup(container);
     Drawing();
+    
+    btnClear.addEventListener(`click`, ()=>{
+    
+        while(container.firstChild){
+            container.removeChild(container.firstChild)
+        }
+        RenderSketch(container, 16,16)
+        Drawing();
+    })
 })()
